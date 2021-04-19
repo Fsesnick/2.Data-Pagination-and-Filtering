112f840e4d7c24e1c6421c535179381ef5ec6690
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-04-19 16:13:01 -0300

    Display an Array of Objects

--- js/script.js
+++ js/script.js
@@ -9,6 +9,25 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+let html = '';
+for(let i =0; i < data.length; i++){
+    let student = data[i];
+    html +=`
+    <li class="student-item cf">
+    <div class="student-details">
+    <img class="avatar" src="${student.picture.medium}" alt="Profile Picture">
+    <h3>${student.name.first} ${student.name.last}</h3>
+    <span class="email">${student.email}</span>
+    </div>
+    <div class="joined-details">
+    <span class="date">${student.registered.date}</span>
+    </div>
+    </li>
+    `;
+}
+//console.log(html);
+
+document.querySelector(".student-list").insertAdjacentHTML('beforeend', html);
 
 
 /*
