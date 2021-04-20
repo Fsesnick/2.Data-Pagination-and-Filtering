8777972ac8b9eb3f7572cef9e17f7627a193a005
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-04-20 09:37:43 -0300

    Final Test

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 /*
 Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
+Fernanda Sesnick
 */
 
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+//Here we can change how many students we want on each page.
 ItemsPerPage = 9;
+
+/**
+* Function: showPage()
+* create and insert/append the elements needed to display a "page" of students
+* @param {array of objects} list - The data of students.
+* @param {Number} page - The number of the page to show.
+**/
 
 function showPage(list, page) {
 
   let startIndex = (page * ItemsPerPage) - ItemsPerPage;
   let endIndex = page * ItemsPerPage;
-     // select the element with a class of `student-list` 
   let studentList = document.querySelector(".student-list");
   studentList.innerHTML ='';
 
@@ -43,37 +47,50 @@ function showPage(list, page) {
     
   }
   
-showPage(data, 1);
 
+/**
+* Function: addPagination()
+* create and insert/append the elements needed for the pagination buttons
+* @param {array of objects} list - The data of students.
+**/
 
+function addPagination(list) {
 
+  let numOfPages = Math.ceil(list.length / ItemsPerPage);
+    // select the element with a class of `link-list` and assign it to a variable
+  let linkList = document.querySelector(".link-list");
+  linkList.innerHTML ='';
+   
+for(i =1; i <= numOfPages; i++){
+      
+      linkList.insertAdjacentHTML("beforeend", `
+      <li>
+        <button type="button">${ i }</button>
+      </li>
+    `);
+}
+    // give the first pagination button a class of "active"
+    linkList.firstElementChild.firstElementChild.className = "active";
+   
+    linkList.addEventListener('click', (evnt) => {
+    
+     if(evnt.target.tagName ==='BUTTON'){
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+        // remove the "active" class from the previous button
+        document.querySelector(".active").className = '';
+        // add the active class to the clicked button
+        evnt.target.className = 'active';
+        let text = evnt.target.textContent;
+        showPage(list, text);
+     }
 
+    });
+
+  }
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
 
-
-/*
-let html = '';
-for(let i =0; i < data.length; i++){
-    let student = data[i];
-    html +=`
-    <li class="student-item cf">
-    <div class="student-details">
-    <img class="avatar" src="${student.picture.medium}" alt="Profile Picture">
-    <h3>${student.name.first} ${student.name.last}</h3>
-    <span class="email">${student.email}</span>
-    </div>
-    <div class="joined-details">
-    <span class="date">${student.registered.date}</span>
-    </div>
-    </li>
-    `;
-}
-//console.log(html);
-*/
+//Aiming for Meets Expectations
